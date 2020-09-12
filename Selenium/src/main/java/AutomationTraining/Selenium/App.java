bc016d745166ad52938a65404b9e3bacb56a45d0
fx-duranc <62355531+fx-duranc@users.noreply.github.com>
2020-09-12 10:15:53 -0500

    initial commit

---
+++ Selenium/src/main/java/AutomationTraining/Selenium/App.java
@@ -0,0 +1,23 @@
+package AutomationTraining.Selenium;
+
+import java.io.File;
+import java.nio.file.Paths;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Curso Automatización 05.09.2020" );
+        String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator;//Tomando el path del driver en el sistema
+        System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");//Setear propiedad del .exe
+        WebDriver driver = new ChromeDriver();//Inicializando el driver
+        driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");//Iniciar Chrome en la URL especificada
+        
+    }
+}
